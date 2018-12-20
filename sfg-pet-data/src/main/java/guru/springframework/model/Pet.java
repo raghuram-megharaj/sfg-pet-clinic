45604c5da90572b30a9b8f84e550dd307a64476a
raghuram-megharaj <raghuram.megharaj@gmail.com>
2018-12-20 16:11:31 +0530

    Initial Project Add

---
+++ sfg-pet-data/src/main/java/guru/springframework/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.springframework.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private String name;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
